cbb1da47b4af845c19edbbbd3638d0aada07d04c
Geoffrey Mwangi <gmwangi3174@gmail.com>
2022-06-17 12:11:31 +0300

    next js app with youtube the net ninja channel

---
+++ comps/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer>copyright &copy; 2022 Ninja List</footer>;
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title> Ninja List | Home</title>
+        <meta name="keywords" content="ninja, list, home" />
+      </Head>
+
+      <div>
+        <h1> About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</p>
+      </div>
+    </>
+  );
+};
+
+export default About;
